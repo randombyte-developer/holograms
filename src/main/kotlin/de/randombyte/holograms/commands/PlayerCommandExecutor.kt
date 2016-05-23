7b5966fdabf567c952342a776c894c81dd765418
randombyte-developer <apps.randombyte@gmail.com>
2016-05-23 16:02:40 +0200

    Throw Exception instead of sending a message and 'successfully' exiting the command

--- src/main/kotlin/de/randombyte/holograms/commands/PlayerCommandExecutor.kt
+++ src/main/kotlin/de/randombyte/holograms/commands/PlayerCommandExecutor.kt
@@ -1,5 +1,6 @@
 package de.randombyte.holograms.commands
 
+import org.spongepowered.api.command.CommandException
 import org.spongepowered.api.command.CommandResult
 import org.spongepowered.api.command.CommandSource
 import org.spongepowered.api.command.args.CommandContext
@@ -12,11 +13,11 @@ import org.spongepowered.api.text.format.TextColors
  * A command that must be executed by a [Player].
  */
 abstract class PlayerCommandExecutor : CommandExecutor {
-    override fun execute(src: CommandSource, args: CommandContext): CommandResult? =
-            if (src !is Player) {
-                src.sendMessage(Text.of(TextColors.RED, "Command must be executed by a player!"))
-                CommandResult.empty()
-            } else executedByPlayer(src, args)
+    override fun execute(src: CommandSource, args: CommandContext): CommandResult {
+        if (src !is Player) throw CommandException(Text.of("Command must be executed by a player!"))
+        return executedByPlayer(src, args)
+    }
+
 
     /**
      * Gets called when this command is executed by a [Player].
