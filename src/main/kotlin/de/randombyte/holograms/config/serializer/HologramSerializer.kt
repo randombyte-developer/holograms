316d8338ad8941ba9b281f8aa191fca54309fbcc
randombyte-developer <apps.randombyte@gmail.com>
2016-05-26 21:14:49 +0200

    Simplify code

--- src/main/kotlin/de/randombyte/holograms/config/serializer/HologramSerializer.kt
+++ src/main/kotlin/de/randombyte/holograms/config/serializer/HologramSerializer.kt
@@ -10,17 +10,13 @@ object HologramSerializer {
 
     val LIST_LIST_STRING_TYPE = object : TypeToken<List<List<String>>>() {}
 
-    fun deserialize(value: ConfigurationNode): List<Pair<UUID, Text>> {
-        val list = value.getValue(LIST_LIST_STRING_TYPE)
-        return list.map { strings ->
-            UUID.fromString(strings[0]) to TextSerializers.FORMATTING_CODE.deserialize(strings[1])
-        }
-    }
+    fun deserialize(value: ConfigurationNode): List<Pair<UUID, Text>> =
+            value.getValue(LIST_LIST_STRING_TYPE).map { strings ->
+                UUID.fromString(strings[0]) to TextSerializers.FORMATTING_CODE.deserialize(strings[1])
+            }
 
-    fun serialize(lines: List<Pair<UUID, Text>>, value: ConfigurationNode): ConfigurationNode {
-        val stringPairs = lines.map { line ->
-            listOf(line.first.toString(), TextSerializers.FORMATTING_CODE.serialize(line.second))
-        }
-        return value.setValue(LIST_LIST_STRING_TYPE, stringPairs)
-    }
+    fun serialize(lines: List<Pair<UUID, Text>>, value: ConfigurationNode): ConfigurationNode =
+            value.setValue(LIST_LIST_STRING_TYPE, lines.map { line ->
+                listOf(line.first.toString(), TextSerializers.FORMATTING_CODE.serialize(line.second))
+            })
 }
