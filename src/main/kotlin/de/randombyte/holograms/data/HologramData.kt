0741581586a73f0eb20fc3797366f731dd502237
randombyte-developer <apps.randombyte@gmail.com>
2017-02-04 23:03:56 +0100

    Updated SpongeStart, Gradle and kosp; added PGP signing; modified HologramData a bit(which breaks the before-saved data :/ )

--- src/main/kotlin/de/randombyte/holograms/data/HologramData.kt
+++ src/main/kotlin/de/randombyte/holograms/data/HologramData.kt
@@ -16,14 +16,13 @@ import org.spongepowered.api.data.manipulator.mutable.common.AbstractData
 import org.spongepowered.api.data.merge.MergeFunction
 import org.spongepowered.api.data.persistence.AbstractDataBuilder
 import org.spongepowered.api.data.persistence.InvalidDataException
-import org.spongepowered.api.data.value.immutable.ImmutableValue
 import org.spongepowered.api.data.value.mutable.Value
 import java.util.*
 
 /**
  * Just to distinguish normal ArmorStands and those that are used as a Hologram.
  */
-class HologramData internal constructor(var isHologram : Boolean = false): AbstractData<HologramData, HologramData.Immutable>() {
+class HologramData internal constructor(var isHologram: Boolean = false) : AbstractData<HologramData, HologramData.Immutable>() {
 
     init {
         registerGettersAndSetters()
@@ -32,23 +31,23 @@ class HologramData internal constructor(var isHologram : Boolean = false): Abstr
     override fun registerGettersAndSetters() {
         registerFieldGetter(HologramKeys.IS_HOLOGRAM, { isHologram })
         registerFieldSetter(HologramKeys.IS_HOLOGRAM, { isHologram = it })
-        registerKeyValue(HologramKeys.IS_HOLOGRAM, { getIsHologramValue() })
+        registerKeyValue(HologramKeys.IS_HOLOGRAM, {
+            Sponge.getRegistry().valueFactory.createValue(HologramKeys.IS_HOLOGRAM, isHologram)
+        })
     }
-
-    fun getIsHologramValue(): Value<Boolean> = Sponge.getRegistry().valueFactory.createValue(HologramKeys.IS_HOLOGRAM, isHologram)
 
     override fun fill(dataHolder: DataHolder, overlap: MergeFunction): Optional<HologramData> {
         dataHolder.get(HologramData::class.java).ifPresent { that ->
-            this.isHologram = overlap.merge(this, that).isHologram
+            isHologram = overlap.merge(this, that).isHologram
         }
-        return this.toOptional()
+        return toOptional()
     }
 
     override fun from(container: DataContainer) = from(container as DataView)
 
     fun from(container: DataView): Optional<HologramData> {
         container.getBoolean(HologramKeys.IS_HOLOGRAM.query).ifPresent { isHologram = it }
-        return this.toOptional()
+        return toOptional()
     }
 
     override fun copy() = HologramData(isHologram)
@@ -59,7 +58,7 @@ class HologramData internal constructor(var isHologram : Boolean = false): Abstr
 
     override fun toContainer(): DataContainer = super.toContainer().set(HologramKeys.IS_HOLOGRAM.query, isHologram)
 
-    class Immutable(val isHologram : Boolean = false) : AbstractImmutableData<Immutable, HologramData>() {
+    class Immutable(val isHologram: Boolean = false) : AbstractImmutableData<Immutable, HologramData>() {
 
         init {
             registerGetters()
@@ -67,17 +66,16 @@ class HologramData internal constructor(var isHologram : Boolean = false): Abstr
 
         override fun registerGetters() {
             registerFieldGetter(HologramKeys.IS_HOLOGRAM, { isHologram })
-            registerKeyValue(HologramKeys.IS_HOLOGRAM, { getIsHologramValue() })
+            registerKeyValue(HologramKeys.IS_HOLOGRAM, {
+                Sponge.getRegistry().valueFactory.createValue(HologramKeys.IS_HOLOGRAM, isHologram).asImmutable()
+            })
         }
-
-        fun getIsHologramValue(): ImmutableValue<Boolean> = Sponge.getRegistry().valueFactory.createValue(HologramKeys.IS_HOLOGRAM, isHologram).asImmutable()
 
         override fun asMutable() = HologramData(isHologram)
 
         override fun getContentVersion() = 1
 
         override fun toContainer(): DataContainer = super.toContainer().set(HologramKeys.IS_HOLOGRAM.query, isHologram)
-
     }
 
     class Builder internal constructor() : AbstractDataBuilder<HologramData>(HologramData::class.java, 1), DataManipulatorBuilder<HologramData, Immutable> {
@@ -88,7 +86,6 @@ class HologramData internal constructor(var isHologram : Boolean = false): Abstr
 
         @Throws(InvalidDataException::class)
         override fun buildContent(container: DataView) = create().from(container)
-
     }
 }
 
@@ -96,5 +93,5 @@ object HologramKeys {
     val IS_HOLOGRAM: Key<Value<Boolean>> = KeyFactory.makeSingleKey(
             Boolean::class.typeToken,
             object : TypeToken<Value<Boolean>>() {},
-            DataQuery.of(".", "is.hologram"), "holograms:is_hologram", "Is Hologram")
+            DataQuery.of("IsHologram"), "holograms:is_hologram", "Is Hologram")
 }
